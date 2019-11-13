bb6d6c68a34bc22b82e27fc074f955bff765434b
leoehler <lennart.oehler@googlemail.com>
2019-11-14 00:02:15 +0100

    Beginnings of the Sea

---
+++ src/de/lingen/modsim/Sea.java
@@ -0,0 +1,31 @@
+package de.lingen.modsim;
+
+public class Sea {
+
+    private SeaTile[][] SeaBoard;
+    private int width;                                      // Breite   [x]
+    private int height;                                     // Höhe     [y]
+
+
+    public Sea (){
+        SeaBoard = new SeaTile[10][10];
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j 10< ; j++) {
+                SeaBoard[i][j] = new SeaTile();
+            }
+        }
+    }
+
+    public Sea (int height, int width){
+        this.width = width;
+        this.height = height;
+        SeaBoard = new SeaTile[width][height];
+
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                SeaBoard[x][y] = new SeaTile();
+            }
+        }
+
+    }
+}
---
+++ src/de/lingen/modsim/SeaTile.java
@@ -0,0 +1,13 @@
+package de.lingen.modsim;
+
+public class SeaTile {
+
+    private boolean water;
+
+    public SeaTile(boolean water){
+        this.water = water;
+    }
+    public SeaTile(){
+        water = false;
+    }
+}
