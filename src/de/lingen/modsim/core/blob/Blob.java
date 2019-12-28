f16d2aa00c315d05b072578685a1c0a12b10e4df
leoehler <lennart.oehler@hs-osnabrueck.de>
2019-12-28 11:55:49 +0100

    move methods

--- src/de/lingen/modsim/core/blob/Blob.java
+++ src/de/lingen/modsim/core/blob/Blob.java
@@ -3,7 +3,7 @@ package de.lingen.modsim.core.blob;
 import de.lingen.modsim.core.Field;
 import de.lingen.modsim.core.Point2D;
 
-import java.rmi.activation.ActivationGroup_Stub;
+import java.util.Random;
 
 public abstract class Blob {
 
@@ -12,6 +12,7 @@ public abstract class Blob {
     private double size;
     private double sense;
     private double food;
+    private final int SCALE = 10;
 
     private Point2D pos;
 
@@ -35,9 +36,10 @@ public abstract class Blob {
     }
 
     public void calculateEnergyCostPerTimeFrame(){
-        //        energy = Math.pow(size,3) + Math.pow(speed,2) + sense;
-        // size^3 + speed^2 + sense
-        energy = (size * size * size) + (speed * speed) + sense;
+            // energy = Math.pow(size,3) + Math.pow(speed,2) + sense;
+            // size^3 + speed^2 + sense
+        //energy = (size * size * size) + (speed * speed) + sense;
+        energy -= ((size * size * size) + (speed * speed) + sense);
     }
 
 
@@ -47,20 +49,75 @@ public abstract class Blob {
      */
     public void move() {
         //TODO move
-        while(energy > energyNeededToReachCorner) {
+        while(energy > energyNeededToReachCorner(pos)) {
+            switch (new Random().nextInt(3)){  //TODO ... OOB check
+                case 0://horizontal R
+                    pos.setX(pos.getX()+speed*SCALE);
+                    break;
+                case 1://horizontal L
+                    pos.setX(pos.getX()-speed*);
+                    break;
+                case 2://vertikal O
+                    pos.setY(pos.getY()+speed);
+                    break;
+                case 3://vertikal U
+                    pos.setY(pos.getY()-speed);
+                    break;
+            }
+            calculateEnergyCostPerTimeFrame();      // Energieverbrauch berechnen & verbrauchen.
 
+            //scan 4 fooodz
+            if (isFoodInRange())  // db abfrage........
+                moveTowardsPoint(pos);
+                if(onFood)      // db abfrage
+                    if(food < 2)
+                        eat();
 
         }
 
         current location check
+            wofür nochmal
 
 
 
-        check 4 foods
-            true = move towards
+    }
 
-            false move ranomdly
+    public void moveTowardsPoint(Point2D destination){
+        //pos = point; // teleport ftw
 
+        while(energy > energyNeededToReachCorner(pos)){
+            //x koordinate
+            if (Math.abs(destination.getX()-pos.getX()) > speed) {
+                if(destination.getX() < pos.getX()) {             // nach links gehen
+                    pos.setX(pos.getX() - speed);
+                    calculateEnergyCostPerTimeFrame();
+                }
+                else if (destination.getX() > pos.getX()) {         // nach rechts gehen
+                    pos.setX(pos.getX() + speed);
+                    calculateEnergyCostPerTimeFrame();
+                }
+            }
+            else {                  // abfrage ob gleich, damit nicht immer in den fall reingesprungen wird?
+                pos.setX(destination.getX());
+                calculateEnergyCostPerTimeFrame();
+            }
+
+            //y koordinate
+            if (Math.abs(destination.getY()-pos.getY()) > speed) {
+                if(destination.getY() < pos.getY()) {             // nach links gehen
+                    pos.setY(pos.getY() - speed);
+                    calculateEnergyCostPerTimeFrame();
+                }
+                else if (destination.getY() > pos.getY()) {         // nach rechts gehen
+                    pos.setY(pos.getY() + speed);
+                    calculateEnergyCostPerTimeFrame();
+                }
+            }
+            else {
+                pos.setY(destination.getY());
+                calculateEnergyCostPerTimeFrame();
+            }
+        }
     }
 
     public abstract void moveStrategy();
@@ -89,11 +146,33 @@ public abstract class Blob {
         this.food = food;
     }
 
+    public void eat(){
+        food++;
+    }
+
     public double energyNeededToReachCorner(Point2D pos){
-        double energy2C = 0;
+        int x = pos.getX();
+        int y = pos.getY();
 
+        Field field = Field.getInstance();
+        int distance = 0;
+        double energy2C = 0;                // clean variant: just return statement further down
+        switch(calcClosestBorder(pos)){
+            case 1:
+                distance = x;
+                break;
+            case 2:
+                distance = field.MAX_Y - y;
+                break;
+            case 3:
+                distance = field.MAX_X - x;
+                break;
+            case 4:
+                distance = y;
+                break;
+        }
+        energy2C = ((size*size*size)+(speed*speed)+sense) * (distance/speed);  //energy cost per time frame * distance covered per time interval
         return energy2C;
-
     }
 
     public int calcClosestBorder(Point2D pos){
@@ -101,18 +180,14 @@ public abstract class Blob {
         int y = pos.getY();
         Field field = Field.getInstance();
 
-        //  Links = 1
-        //  Oben = 2
-        //  Rechts = 3
-        //  Unten = 4
-
-        if (x <= field.MAX_X-x && x <= y && x <= field.MAX_Y-y)
+        if (x <= field.MAX_X-x && x <= y && x <= field.MAX_Y-y)                                 //Left
             return 1;
-        if (field.MAX_X-x <= x && field.MAX_X-x <= y && field.MAX_X-x <= field.MAX_Y-y)
+        if (field.MAX_X-x <= x && field.MAX_X-x <= y && field.MAX_X-x <= field.MAX_Y-y)         //Right
             return 3;
-        if (y <= field.MAX_Y-y && y <= x && y <= field.MAX_X-x)
+        if (y <= field.MAX_Y-y && y <= x && y <= field.MAX_X-x)                                 //Bottom
             return 4;
-        if (field.MAX_Y-y <= y && field.MAX_Y-y <= x && field.MAX_Y-y <= field.MAX_X-x)
+        if (field.MAX_Y-y <= y && field.MAX_Y-y <= x && field.MAX_Y-y <= field.MAX_X-x)         //Top
             return 2;
+        return 1;
     }
 }
